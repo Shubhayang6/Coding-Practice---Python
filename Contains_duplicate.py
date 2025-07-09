ab1f2d2ae9309488af393cb51c1a29c7b8ce02e5
Shubhayan Ghosh <shubhayan.tech@gmail.com>
2025-07-09 19:18:21 +0530

    Add leetcode 217

--- Contains_duplicate.py
+++ Contains_duplicate.py
@@ -1,6 +1,8 @@
 # Leetcode - 217
 
 def containsDuplicate(): 
+    # Time - O(n)
+    # Space - O(n)
     new_set = set()
     for i in range(len(nums)):
         if nums[i] in new_set:
@@ -8,6 +10,15 @@ def containsDuplicate():
         else:
             new_set.add(nums[i])
     return False
+
+    # Time - O(n)
+    # Space - O(n)
+    nums.sort()
+    for i in range(len(nums)):
+        if nums[i] == nums[i-1]:
+            return True
+    return False
+
 
 
 if __name__ == "__main__":
